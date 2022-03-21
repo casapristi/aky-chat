ba22d97ce1b9264d824e28d70d3a7f3561e83fe0
casapristi <64585816+casapristi@users.noreply.github.com>
2022-03-21 21:44:52 +0100

    Update chat.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -222,7 +222,7 @@ function zoomImage(image){
   imageInfo.textContent=`Name: ${image.name} - Size: ${image.size}px`;
   screenOverlay.hidden=false;
 };
-function generateAvatar(username){
+function generateAvatar(username="Unknown"){
   const alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   const colors=[
     "#49e744",
